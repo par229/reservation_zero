de9fa3821e86361285b3e3747ac74dadb0ced2c0
blueoxygens <exboxer234@gmail.com>
2025-03-28 03:58:30 +0900

    feat: Initial file created

---
+++ server/src/main/java/com/zeroton/reservation/reservation_zeroton/ReservationZerotonApplication.java
@@ -0,0 +1,13 @@
+package com.zeroton.reservation.reservation_zeroton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationZerotonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationZerotonApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/zeroton/reservation/reservation_zeroton/ReservationZerotonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeroton.reservation.reservation_zeroton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationZerotonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
