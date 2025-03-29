ea6eb5a669c7005812de4e0e98d11c450a48fda9
l-wanderer01 <jhjjang0423@naver.com>
2025-03-29 14:42:19 +0900

    feat: 강의 추가, 삭제, 검색 로직 추가

--- Backend/demos/urls.py
+++ Backend/demos/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="login"),  # JWT 로그인
     path("logout/", views.logout, name="logout"),
     path("addclass/", classroom.addclass, name="addclass"),
+    path("lecture/create/", views.create_lecture, name="create_lecture"),
+    path("lecture/delete/", views.delete_lecture, name="delete_lecture"),
+    path("lecture/find/", views.find_lecture, name="search_lecture"),
     #path("deleteclass/", views.deleteclass, name="deleteclass"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- Backend/demos/views.py
+++ Backend/demos/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate
-from demos.models import CustomUser
+from demos.models import CustomUser, Lecture
 from django.http import JsonResponse
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.views.decorators.csrf import csrf_exempt
@@ -60,3 +60,62 @@ def signup(request):
             'refresh': str(refresh)
         })
     return JsonResponse({'status': 405, 'error': 'Method not allowed'}, status=405)
+
+
+@csrf_exempt
+def create_lecture(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        name = data.get('name')
+
+        if not name:
+            return JsonResponse({"error": "You should enter the name value"}, status=400)
+
+        if Lecture.objects.filter(name=name).exists():
+            return JsonResponse({"error": "already exist lecture"}, status=400)
+
+        Lecture.objects.create(name=name)
+        return JsonResponse({"message": f"lecture: '{name}' is created"}, status=201)
+    
+    return JsonResponse({"error": "wrong request"}, status=400)
+
+
+@csrf_exempt
+def delete_lecture(request):
+    if request.method == 'DELETE':
+        data = json.loads(request.body)
+        name = data.get('name')
+
+        if not name:
+            return JsonResponse({"error": "You should enter the name value"}, status=400)
+
+        lecture_qs = Lecture.objects.filter(name=name)
+        if not lecture_qs.exists():
+            return JsonResponse({"error": "the lecture dosen't exist"}, status=404)
+
+        lecture_qs.delete()
+        return JsonResponse({"message": f"lecture: '{name}' is deleted"}, status=200)
+    
+    return JsonResponse({"error": "wrong request"}, status=400)
+
+
+@csrf_exempt
+def find_lecture(request):
+    if request.method == 'GET':
+        data = json.loads(request.body)
+        lecture_name = data.get('name')
+
+        # 강의 입력 여부
+        if not lecture_name:
+            return JsonResponse({"error": "name은 필수 입력 값입니다."}, status=400)
+        
+        lecture_name = Lecture.objects.filter(name = lecture_name).first()
+
+        if not lecture_name:
+            return JsonResponse({"error": "존재하지 않는 강의입니다."}, status=400)
+        
+        # 강의 검색 성공
+        return JsonResponse({"message": f"강의'{lecture_name.name}'가 검색되었습니다."}, status=200)
+    
+    return JsonResponse({"error": "잘못된 요청입니다."}, status=400)
