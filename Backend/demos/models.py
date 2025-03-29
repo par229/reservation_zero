1e3f4692d8d2cdfb39ad1fe92af8664bc153b952
blueoxygens <exboxer234@gmail.com>
2025-03-29 12:58:13 +0900

    feat: django backend 구축. signup, login, logout 구현 완료

---
+++ Backend/demos/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and return a user with an email as the username.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create and return a superuser with an email as the username.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = None  # username 필드를 제거
+    email = models.EmailField(unique=True)  # 이메일을 아이디로 사용
+    name = models.CharField(max_length=10, null=True, blank=True)
+    nickname = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    birth = models.DateField(null=True, blank=True)
+
+    objects = CustomUserManager()  # 커스텀 매니저 사용
+
+    USERNAME_FIELD = 'email'  # 로그인 시 이메일을 사용
+    REQUIRED_FIELDS = ['name', 'nickname', 'birth']  # 필수 필드 설정
+  
+class Lecture(models.Model):
+    name = models.CharField(max_length=100)
+
+class ClassRoom(models.Model):
+    course_name = models.ForeignKey(Lecture, on_delete=models.CASCADE, related_name="live_info")
+    classroom_location = models.CharField(max_length=100)
+    class_time = models.DateTimeField()
+    capacity = models.IntegerField()
+    current_students = models.IntegerField()
+
+    current_space = ArrayField(models.BooleanField(default=False), blank=True, default=list)
+
+    def save(self, *args, **kwargs):
+        # current_space를 capacity만큼의 False 배열로 설정
+        if not self.current_space:
+            self.current_space = [False] * self.capacity
+        super(ClassRoom, self).save(*args, **kwargs)
---
+++ Backend/demos/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
+
+app_name = "demos"
+
+urlpatterns = [
+    path("signup/", views.signup, name="signup"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),  # JWT 로그인
+    path("logout/", views.logout, name="logout"),
+    #path("addclass/", views.addclass, name="addclass"),
+    #path("deleteclass/", views.deleteclass, name="deleteclass"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
