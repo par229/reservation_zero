aa2d3e513729c1d398652acff124cdbe362e99f5
blueoxygens <exboxer234@gmail.com>
2025-03-29 15:52:52 +0900

    fix: 중복 코드 삭제

--- Backend/demos/urls.py
+++ Backend/demos/urls.py
@@ -13,9 +13,11 @@ urlpatterns = [
     path("updateclass/", classroom.updateclass, name="updateclass"),
     path("getclass/", classroom.getclass, name ="getclass"),
     path("getallclasses/", classroom.getallclasses, name="getallclasses"),
+
     path('make_reservation/', reservation.make_reservation, name='make_reservation'),
     path('delete_reservation/', reservation.delete_reservation, name='delete_reservation'),
     path('view_reservations/', reservation.view_reservations, name='view_reservations'),
+
     path("lecture/create/", lecture.create_lecture, name="create_lecture"),
     path("lecture/delete/", lecture.delete_lecture, name="delete_lecture"),
     path("lecture/find/", lecture.find_lecture, name="search_lecture"),
